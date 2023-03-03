22ef0f91a0e7152b4e29f73ae401a23f05ee0064
EmmanuelJessi <107475789+EmmanuelJessi@users.noreply.github.com>
2023-03-03 23:32:44 +0300

    login and welcome

---
+++ src/LogPage/lognPage.java
@@ -0,0 +1,13 @@
+package LogPage;
+
+import javax.swing.*;
+
+public class lognPage {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JScrollBar scrollBar1;
+    private JScrollBar scrollBar2;
+    private JScrollBar scrollBar3;
+    private JScrollBar scrollBar4;
+    private JScrollBar scrollBar5;
+}
